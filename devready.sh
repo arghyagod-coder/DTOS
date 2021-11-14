18dcf3d1c3c574ffb6e37dae938fb81cff5c3614
Arghya Sarkar <76429451+arghyagod-coder@users.noreply.github.com>
2021-11-14 22:57:23 +0530

    Update devready.sh

--- devready.sh
+++ devready.sh
@@ -31,8 +31,8 @@ addkeyserver() { \
     echo "#######################################################"
     echo "## Adding keyservers to /etc/pacman.d/gnupg/gpg.conf ##"
     echo "#######################################################"
-    grep -qxF "keyserver.ubuntu.com:80" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkp://keyserver.ubuntu.com:80" | sudo tee -a /etc/pacman.d/gnupg/gpg.conf
-    grep -qxF "keyserver.ubuntu.com:443" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkps://keyserver.ubuntu.com:443" | sudo tee -a /etc/pacman.d/gnupg/gpg.conf
+    grep -qxF "keyserver.ubuntu.com:80" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkp://keyserver.ubuntu.com:80" | tee -a /etc/pacman.d/gnupg/gpg.conf
+    grep -qxF "keyserver.ubuntu.com:443" /etc/pacman.d/gnupg/gpg.conf || echo "keyserver hkps://keyserver.ubuntu.com:443" | tee -a /etc/pacman.d/gnupg/gpg.conf
 }
 
 addkeyserver || error "Error adding keyservers to /etc/pacman.d/gnupg/gpg.conf"
@@ -42,8 +42,8 @@ receive_key() { \
     echo "#####################################"
     echo "## Adding PGP key $_pgpkey ##"
     echo "#####################################"
-    sudo pacman-key --recv-key $_pgpkey
-    sudo pacman-key --lsign-key $_pgpkey
+    pacman-key --recv-key $_pgpkey
+    pacman-key --lsign-key $_pgpkey
 }
 
 receive_key || error "Error receiving PGP key $_pgpkey"
